cc92502d15cda8321061a1dca69120580debbafb
XRogo <gamesxrogo@gmail.com>
2025-04-27 23:25:01 +0200

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000');
+const socket = io();
 
 const buttons = document.querySelectorAll('.menu-button');
 const okButtons = document.querySelectorAll('.ok-button');
