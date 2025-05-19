f245839bf7cc723dbde124111cd38a2aca48963b
XRogo <gamesxrogo@gmail.com>
2025-05-19 18:27:18 +0200

    dsadas

--- public/gwent/game.js
+++ public/gwent/game.js
@@ -149,8 +149,8 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         if (collectionArea) {
-            const areaLeft = backgroundLeft + (346 / GUI_WIDTH) * backgroundWidth;
-            const areaWidth = ((1580 - 346) / GUI_WIDTH) * backgroundWidth;
+            const areaLeft = backgroundLeft + (331 / GUI_WIDTH) * backgroundWidth;
+            const areaWidth = ((1591 - 331) / GUI_WIDTH) * backgroundWidth;
             collectionArea.style.left = `${areaLeft}px`;
             collectionArea.style.width = `${areaWidth}px`;
             updateCardArea(
@@ -164,9 +164,9 @@ document.addEventListener('DOMContentLoaded', () => {
         if (deckArea) {
             updateCardArea(
                 deckArea,
-                backgroundLeft + (2270 / GUI_WIDTH) * backgroundWidth,
+                backgroundLeft + (2255 / GUI_WIDTH) * backgroundWidth,
                 backgroundTop + (471 / GUI_HEIGHT) * backgroundHeight,
-                ((3504 - 2270) / GUI_WIDTH) * backgroundWidth,
+                ((3519 - 2270) / GUI_WIDTH) * backgroundWidth,
                 (1940 / GUI_HEIGHT) * backgroundHeight // <-- TO JEST DOBRZE!
             );
         }
