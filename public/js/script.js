686d11eeed34304dab763379e9786a763cf299c6
XRogo <gamesxrogo@gmail.com>
2025-07-28 03:56:58 +0200

    hgdffhhgdffhhgdgdf

--- public/js/script.js
+++ public/js/script.js
@@ -318,24 +318,6 @@ function resetGameState() {
     }
 }
 
-// Funkcje do zapisu i odczytu talii w localStorage
-// frakcja: string np. "1", dowodca: string np. "0004", karty: tablica np. ["001", "123", "123", "234"]
-function saveDeck(frakcja, dowodca, karty) {
-    let talie = {};
-    try {
-        talie = JSON.parse(localStorage.getItem('gwint_talie')) || {};
-    } catch(e) {}
-    talie[frakcja] = { dowodca, karty };
-    localStorage.setItem('gwint_talie', JSON.stringify(talie));
-}
-
-// Zwraca obiekt: { frakcja: { dowodca, karty } }
-function loadDecks() {
-    try {
-        return JSON.parse(localStorage.getItem('gwint_talie')) || {};
-    } catch(e) { return {}; }
-}
-
 socket.on('join-success', (data) => {
     console.log('Dołączono do gry:', data.gameCode);
     isJoined = true;
